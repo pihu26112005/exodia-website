a3059eb34b3da9de602ceaabc2b35103f58cffa1
pihu26112005 <piyushkumar26november@gmail.com>
2025-04-11 19:45:37 +0530

    minute fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 
-import "@uploadthing/react/styles.css";
+// import "@uploadthing/react/styles.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -71,7 +71,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main className="flex-grow relative z-10">
           <ToasterProvider />
           {children}
-          </main>
+        </main>
         <Footer />
         <HorrorTimer />
       </body>
--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import { FocusCards } from '../components/focus-cards';
+import { teamsList } from '@/lib/utils';
 
 
 
