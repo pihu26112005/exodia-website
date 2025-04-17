0bfca70e2e5218d3147632c90a3b4ba3b3cc9d8b
Anshul <anshulmendiratta10d@gmail.com>
2025-04-17 21:43:55 +0530

    Announcements GET API Creation

--- app/api/announcements/route.ts
+++ app/api/announcements/route.ts
@@ -1,34 +1,40 @@
 import { connectToDatabase } from "@/lib/connectDB";
+
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
 
-  // Validating incoming request data
-  if (!body || !body.title || !body.description) {
+  if(!body || !body.title || !body.description)
     return new NextResponse("Invalid data", { status: 400 });
-  }
 
   try {
-    // Establish the database connection
     const db = await connectToDatabase();
 
     const createdAt = new Date();
 
-    const newAnnouncement = {
-      title: body.title,
-      description: body.description,
-      imageUrl: body.imageUrl,
-      time: createdAt,
-    };
+    const newAnnouncement = { title: body.title, description: body.description, imageUrl: body.imageUrl, time: createdAt };
 
-    // Insert the new announcement document
     const result = await db.collection("announcements").insertOne(newAnnouncement);
 
-    // Respond with the created announcement
     return NextResponse.json({ ...newAnnouncement, _id: result.insertedId });
-  } catch (error) {
-    console.error("Database operation failed:", error);
+  }
+  catch (error) {
+    console.error("[ANNOUNCEMENTS_POST]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const db = await connectToDatabase();
+
+    const announcements = await db.collection("announcements").find().toArray();
+
+    return NextResponse.json(announcements);
+  }
+  catch(error) {
+    console.error("[ANNOUNCEMENTS_GET]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
